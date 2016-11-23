acc023da55c0beed58dc2bf86d1af5a34e00b617
Nick Malaguti <nmalaguti@users.noreply.github.com>
2016-11-23 17:21:29 -0500

    Add JAR file instructions
    Closes #254

--- website/basics_quickstart.php
+++ website/basics_quickstart.php
@@ -16,7 +16,7 @@
             <div class="col-sm-9">
                 <h1>Getting Started</h1>
                 <p>In this short tutorial, we will explain how to submit your first bot and get your name on the Halite leaderboard.</p>
-                
+
                 <h3>Register using Github</h3>
 
                 <p>You will need a Github account in order to participate in the Halite tournament. If you don't have a Github account, please create a new account <a href="https://github.com/join"> here</a>. Sign up for Halite by clicking "Login with Github" at the top right of this page.</p>
@@ -24,8 +24,8 @@
                 <h3>Download a Starter Package</h3>
 
                 <p>Download a starter package for your language of choice from the <a href="downloads.php">downloads page</a>.</p>
-                
-                <p>Each starter package consists of a zip file that includes: (a) one code template for your your first bot (the MyBot file), (b) one code sample for a random bot (the RandomBot file), (c) some support libraries for the bot and (d) some tools to test your bot locally.</p> 
+
+                <p>Each starter package consists of a zip file that includes: (a) one code template for your your first bot (the MyBot file), (b) one code sample for a random bot (the RandomBot file), (c) some support libraries for the bot and (d) some tools to test your bot locally.</p>
 
                 <h3>Getting on the Leaderboard</h3>
 
@@ -33,6 +33,10 @@
                 <p>Click the "Submit" button in the navigation bar. In general, you need to package your submission as a zip file that contains everything needed to run your bot (your bot’s source code, starter package source files, supplemental libraries, etc.). For your first submission, you can simply upload the zipped starter package that you just downloaded. You will receive an email notification that your submission has been received.</p>
                 <p>The Halite servers will compile the source code of your submission and continuously play your bot against other contestants, which will generate your leaderboard ranking. If the compilation of your source code fails, we will email you.</p>
                 <p>To track your bot's progress, navigate to your <a href="user.php">homepage</a>.</p>
+
+                <h4>Submitting a JAR</h4>
+
+                <p>For JVM languages, you can submit a jar file inside of your zip file instead of source files. The jar will be executed <code>java -jar MyBot.jar</code> so you need to <a href="https://docs.oracle.com/javase/tutorial/deployment/jar/appman.html">define a Main-Class header in the manifest</a>.</p>
 
                 <h3>Next Steps</h3>
                 <p>Congratulations. You are now an official contestant!</p>
