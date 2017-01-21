41eaa1e8dc695bcbb8e4d232931b82bcaf3f56e7
Nick Malaguti <nmalaguti@users.noreply.github.com>
2017-01-21 14:49:17 -0500

    Add /O2

--- environment/make.bat
+++ environment/make.bat
@@ -1,4 +1,4 @@
 SET PATH=C:\Program Files (x86)\MSBuild\14.0\Bin;%PATH%
 CALL "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
 cd environment
-cl.exe /EHsc /o2 main.cpp core/Halite.cpp /I . networking/Networking.cpp /link /out:halite.exe
+cl.exe /O2 /EHsc main.cpp core/Halite.cpp /I . networking/Networking.cpp /link /out:halite.exe
