a0533760b246fdf50b9effc5526a8b1e8b1d531d
Nick Malaguti <nmalaguti@users.noreply.github.com>
2017-01-21 14:32:40 -0500

    Make make.bat work on windows

--- environment/make.bat
+++ environment/make.bat
@@ -1,1 +1,1 @@
-g++ -o2 -std=c++11 main.cpp core/Halite.cpp -I . networking/Networking.cpp -o halite.exe -static
+cl.exe /EHsc /o2 main.cpp core/Halite.cpp /I . networking/Networking.cpp /link /out:halite.exe
