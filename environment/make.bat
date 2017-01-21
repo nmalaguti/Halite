7355e561121c6e547ec66e1dad66638c159b8bdf
Nick Malaguti <nmalaguti@users.noreply.github.com>
2017-01-21 14:39:40 -0500

    Change path

--- environment/make.bat
+++ environment/make.bat
@@ -1,1 +1,3 @@
+SET PATH=C:\Program Files (x86)\MSBuild\14.0\Bin;%PATH%
+CALL "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
 cl.exe /EHsc /o2 main.cpp core/Halite.cpp /I . networking/Networking.cpp /link /out:halite.exe
